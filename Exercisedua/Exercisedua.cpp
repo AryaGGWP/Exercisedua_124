2069c116b5d328429881018f3b2f17fa9b942f14
KeikooUwU <127074149+KeikooUwU@users.noreply.github.com>
2023-06-05 10:43:40 +0700

    last

--- Exercisedua/Exercisedua.cpp
+++ Exercisedua/Exercisedua.cpp
@@ -1,20 +1,53 @@
-// Exercisedua.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <string>
+
+class Penerbit {
+private:
+    std::string namaPenerbit;
+
+public:
+    Penerbit(const std::string& nama) : namaPenerbit(nama) {}
+
+    std::string getNamaPenerbit() const {
+        return namaPenerbit;
+    }
+};
+
+class Pengarang {
+private:
+    std::string namaPengarang;
+    Penerbit* penerbit;
+
+public:
+    Pengarang(const std::string& nama, Penerbit* pub) : namaPengarang(nama), penerbit(pub) {}
+
+    std::string getNamaPengarang() const {
+        return namaPengarang;
+    }
+
+    Penerbit* getPenerbit() const {
+        return penerbit;
+    }
+};
+
+class Buku {
+private:
+    std::string judulBuku;
+    Pengarang* pengarang;
+
+public:
+    Buku(const std::string& judul, Pengarang* author) : judulBuku(judul), pengarang(author) {}
+
+    std::string getJudulBuku() const {
+        return judulBuku;
+    }
+
+    Pengarang* getPengarang() const {
+        ret
+
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
